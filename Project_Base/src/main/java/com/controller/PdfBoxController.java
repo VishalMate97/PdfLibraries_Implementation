18bb6d999713dff9dcfcc42c2972f90e9703fa73
Vishal Mate <vishalmate27597@gmail.com>
2023-02-03 01:19:28 +0530

    Edit Existing PDF

--- Project_Base/src/main/java/com/controller/PdfBoxController.java
+++ Project_Base/src/main/java/com/controller/PdfBoxController.java
@@ -1,24 +1,31 @@
 package com.controller;
 
 import java.io.File;
+import java.io.IOException;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.encryption.InvalidPasswordException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.model.requestParam;
+
 @RestController
 public class PdfBoxController {
 
-	@GetMapping("/savePdf")
+	@GetMapping("/addNewPdf")
 	@ResponseBody
-	public String firstPdfBoxAPI(@RequestParam String path1) {
-
+	public String createAndAddPageInPdfAPI(@RequestBody requestParam params) {
+		
+//		create new File If doesn't exist or it will replace If exists
 //		String path = "src"+File.separator+"main"+File.separator+"resources"+File.separator+"document"+File.separator+"myfile.pdf";
-		String path = "src/main/resources/document/myfile.pdf";//create new File If doesn't exist or it will replace If exists
+//		String path = "src/main/resources/document/myfile.pdf";//create new File If doesn't exist or it will replace If exists
 //		String path = "D:\\Temp\\Documents\\myfile.pdf";
+		String path = params.getPath();
 		PDDocument doc = new PDDocument();
 		PDPage p1 = new PDPage();
 		try {
@@ -31,5 +38,21 @@ public class PdfBoxController {
 			return "Unable to save PDF at " + path;
 		}
 		return "Empty pdf is saved at " + path;
+	}
+	
+	@GetMapping("/editExistingPdf")
+	@ResponseBody
+	public String editExistingPdfAPI(@RequestBody requestParam param) {
+		PDDocument doc;
+		try {
+			doc = PDDocument.load(new File(param.getPath1())); // load Existing PDF file through static load method
+			doc.addPage(new PDPage()); // make changes into that existing pdf, here we are adding pages 
+			doc.save(param.getPath2());
+			doc.close();
+		} catch (Exception | Error ex) {
+			ex.printStackTrace();
+			return "Unable to save Edited PDF";
+		}
+		return "Edited PDF is saved";
 	}
 }
---
+++ Project_Base/src/main/java/com/model/requestParam.java
@@ -0,0 +1,35 @@
+package com.model;
+
+public class requestParam {
+
+	private String path;
+
+	private String path1;
+
+	private String path2;
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	public String getPath1() {
+		return path1;
+	}
+
+	public void setPath1(String path1) {
+		this.path1 = path1;
+	}
+
+	public String getPath2() {
+		return path2;
+	}
+
+	public void setPath2(String path2) {
+		this.path2 = path2;
+	}
+	
+}
