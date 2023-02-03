a5d7763f8f78f34ffb3f72e86d6765d1e00fc15f
Vishal Mate <vishalmate27597@gmail.com>
2023-02-03 12:33:54 +0530

    Load And Edit Existing PDF (PDFBOX)

--- Project_Base/src/main/java/com/controller/PdfBoxController.java
+++ Project_Base/src/main/java/com/controller/PdfBoxController.java
@@ -5,7 +5,9 @@ import java.io.IOException;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.encryption.InvalidPasswordException;
+import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -40,6 +42,22 @@ public class PdfBoxController {
 		return "Empty pdf is saved at " + path;
 	}
 	
+	@GetMapping("/loadExistingPdf")
+	@ResponseBody
+	public String loadExistingPdfAPI(@RequestBody requestParam param) {
+		PDDocument doc;
+		try {
+			doc = PDDocument.load(new File(param.getPath1())); // load Existing PDF file through static load method
+			doc.addPage(new PDPage()); // make changes into that existing pdf, here we are adding pages 
+			doc.save(param.getPath2());
+			doc.close();
+		} catch (Exception | Error ex) {
+			ex.printStackTrace();
+			return "Unable to save Edited PDF";
+		}
+		return "Edited PDF is saved";
+	}
+	
 	@GetMapping("/editExistingPdf")
 	@ResponseBody
 	public String editExistingPdfAPI(@RequestBody requestParam param) {
@@ -47,7 +65,18 @@ public class PdfBoxController {
 		try {
 			doc = PDDocument.load(new File(param.getPath1())); // load Existing PDF file through static load method
 			doc.addPage(new PDPage()); // make changes into that existing pdf, here we are adding pages 
-			doc.save(param.getPath2());
+			
+			PDPage p1 = doc.getPage(1);
+			PDPageContentStream contentStream = new PDPageContentStream(doc, p1);
+			contentStream.beginText();
+			contentStream.setFont(PDType1Font.TIMES_BOLD_ITALIC, 14);
+			contentStream.newLineAtOffset(20, 450);
+			String text = "We Are Inside PDF From PDFBOX API editExistingPdf";
+			contentStream.showText(text);
+			contentStream.endText();
+			contentStream.close();
+			
+			doc.save(param.getPath1());
 			doc.close();
 		} catch (Exception | Error ex) {
 			ex.printStackTrace();
